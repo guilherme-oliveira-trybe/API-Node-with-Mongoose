92d15b40f1dcd760f7d7199e6d5b91c19d386540
Guilherme Oliveira <guilherme.oliveira@pxbank.com.br>
2024-01-18 18:27:36 -0300

    config: instala prettier e finaliza configuração do inicial do projeto

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => console.log(
-  `Server is running on PORT: ${PORT}`,
-));
+const server = app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
 
 export default server;
