84f4a3b301e7d7d61f721e469737660c31576f19
Guilherme Oliveira <guilherme.oliveira@pxbank.com.br>
2024-01-18 21:34:22 -0300

    config: mongoose e arquivo de conexão

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import connectToDatabase from './models/connection';
 
 const app = express();
 
@@ -6,6 +7,15 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+const server = connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+  })
+  .catch((error) => {
+    console.log('Connection with database generated an error:\r\n');
+    console.error(error);
+    console.log('\r\nServer initialization cancelled');
+    process.exit(0);
+  });
 
 export default server;
