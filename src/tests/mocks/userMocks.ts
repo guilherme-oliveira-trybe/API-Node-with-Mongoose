32ef4f4981a6a49570372031a62a51332e7893f9
Guilherme Oliveira <guilherme.oliveira@pxbank.com.br>
2024-01-23 14:22:30 -0300

    feat: Adiciona testes unitários a camada de model

--- src/tests/mocks/userMocks.ts
+++ src/tests/mocks/userMocks.ts
@@ -13,4 +13,45 @@ const userMockWithId: IUser & { _id: string } = {
   password: '123456',
 };
 
-export { userMock, userMockWithId };
+const allUsersMock: IUser[] & { _id: string }[] = [
+  {
+    _id: '62cf1fc6498565d94eba52cd',
+    displayName: 'John Doe',
+    email: 'john@email.com',
+    password: '123456',
+  },
+  {
+    _id: '73cf1fc6498565d94eba52cd',
+    displayName: 'Doe John',
+    email: 'doejohn@email.com',
+    password: '654321',
+  },
+  {
+    _id: '84cf1fc6498565d94eba52cd',
+    displayName: 'Doe John Doe',
+    email: 'doejohndoe@email.com',
+    password: '854729',
+  },
+];
+
+const userChangeMock: IUser = {
+  displayName: 'Usuário que mudou de nome',
+  email: 'mudou@email.com',
+  password: '8754785',
+};
+
+const userChangeMockWithId: IUser & { _id: string } = {
+  _id: '62cf1fc6498565d94eba52cd',
+  displayName: 'Usuário que mudou de nome',
+  email: 'mudou@email.com',
+  password: '8754785',
+};
+
+const userDeletedMockWithId: IUser & { _id: string } = {
+  _id: '62cf1fc6498565d94eba52cd',
+  displayName: 'Usuário que foi deletado',
+  email: 'deletou@email.com',
+  password: '8754785',
+};
+
+export { userMock, userMockWithId, allUsersMock, userChangeMock, userChangeMockWithId, userDeletedMockWithId };
--- src/tests/unit/models/userModel.test.ts
+++ src/tests/unit/models/userModel.test.ts
@@ -1,6 +1,92 @@
-describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    const sum = 1 + 2;
-    expect(sum).toBe(3)
+import UserModel from '../../../models/User';
+import { userMock, userMockWithId, allUsersMock, userChangeMock, userChangeMockWithId, userDeletedMockWithId } from '../../mocks/userMocks';
+import { ErrorTypes } from '../../../errors/catalog';
+
+jest.mock('../../../models/User.ts', () => {
+  return jest.fn().mockImplementation(() => ({
+    create: jest.fn(() => userMockWithId),
+    read: jest.fn().mockReturnValueOnce(allUsersMock).mockReturnValueOnce([]),
+    readOne: jest.fn().mockReturnValueOnce(userMockWithId).mockRejectedValueOnce(new Error(ErrorTypes.InvalidMongoId)),
+    update: jest.fn().mockReturnValueOnce(userChangeMockWithId).mockRejectedValueOnce(new Error(ErrorTypes.InvalidMongoId)),
+    delete: jest.fn().mockReturnValueOnce(userDeletedMockWithId).mockRejectedValueOnce(new Error(ErrorTypes.InvalidMongoId)),
+  }));
+});
+
+describe('User Model', () => {
+  const userModel = new UserModel();
+
+  describe('creating a user', () => {
+    test('successfully created', async () => {
+      const newUser = await userModel.create(userMock);
+      expect(newUser).toBe(userMockWithId);
+    });
+  });
+
+  describe('searching all users', () => {
+    test('successfully found', async () => {
+      const allUsersFound = await userModel.read();
+      expect(allUsersFound).toStrictEqual(allUsersMock);
+    });
+
+    test('if dont have users, returns an empty list', async () => {
+      const allUsersFound = await userModel.read();
+      expect(allUsersFound).toEqual([]);
+    });
+  });
+
+  describe('searching a user', () => {
+    test('successfuly found', async () => {
+      const userFound = await userModel.readOne(userMockWithId._id);
+      expect(userFound).toBe(userMockWithId);
+    });
+
+    test('_id invalid', async () => {
+      let error;
+      try {
+        await userModel.readOne('123ERRADO')
+      } catch (err: any) {
+        error = err.message
+      }
+      expect(error).toBe(ErrorTypes.InvalidMongoId)
+    });
+  });
+
+  describe('changing a user', () => {
+    it('successfully changed', async () => {
+      const userChanged = await userModel.update(userChangeMockWithId._id, userChangeMock);
+
+      expect(userChanged).toBe(userChangeMockWithId);
+    })
+
+    it('_id invalid', async () => {
+      let error;
+      try {
+        await userModel.update('123ERRADO', userChangeMock);
+      } catch (err: any) {
+        error = err.message
+      }
+
+      expect(error).toBe(ErrorTypes.InvalidMongoId);
+    })
   })
-})
+
+  describe('delete a user', () => {
+    it('successfully delete', async () => {
+      const userDeleted = await userModel.delete(userDeletedMockWithId._id);
+
+      expect(userDeleted).toBe(userDeletedMockWithId);
+    })
+
+    it('_id invalid', async () => {
+      let error;
+      try {
+        await userModel.delete('123ERRADO');
+      } catch (err: any) {
+        error = err.message
+      }
+
+      expect(error).toBe(ErrorTypes.InvalidMongoId);
+    })
+  })
+
+});
